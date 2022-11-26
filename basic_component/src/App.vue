12006d22fffbcdf0aaa1a6dae00e7ae54071d868
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-11-26 20:42:22 +0900

    propsに関する学習

---
+++ basic_component/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <NinjaCode text="Hello NinjaCode!">
+    </NinjaCode>
+  </div>
+</template>
+
+<script>
+import NinjaCode from './components/NinjaCode.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NinjaCode
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ basic_component/src/components/NinjaCode.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is NinjaCode Component!</h1>
+    <h2>{{ text }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NinjaCode',
+  props: ['text'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
