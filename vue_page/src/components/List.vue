df13fc7c364c5c4e878fc33fbdeefcc036e86a7a
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-12-02 07:51:05 +0900

    ファイル追加

--- vue_page/src/components/List.vue
+++ vue_page/src/components/List.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="main-list-wrapper">
-    <ul class="main-list">
-      <Listitem v-for="dream in dreams" :dream="dream" :key="dream.id"></Listitem>
-    </ul>
+  <div>
+    <div class="main-form-wrapper">
+      <form v-on:submit.prevent="addDream">
+        <input type="text" v-model="newDreamName" class="form-input" placeholder="あなたの夢を追加してください">
+        <input type="submit" class="form-submit">
+      </form>
+    </div>
+    <div class="main-list-wrapper">
+      <ul class="main-list">
+        <Listitem v-for="dream in dreams" :dream="dream" :key="dream.id"></Listitem>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -36,13 +44,21 @@ export default {
           id: 5,
           name: "スティーブ・ジョブズになりたい"
         },
-      ]
+      ],
+      newDreamName: "",
+    }
+  },
+  methods: {
+    addDream: function() {
+      const last_id = this.dreams[this.dreams.length -1].id += 1;
+      this.dreams.push({id: last_id, name: this.newDreamName});
+      this.newDreamName = "";
     }
   }
 }
 </script>
 <style>
-  .main-list-wrapper{
+  .main-list-wrapper, .main-form-wrapper{
     background: #fff;
     width: 80%;
     margin: 2rem auto;
@@ -51,5 +67,20 @@ export default {
   .main-list {
     padding: 2rem;
     text-align: left;
+  }
+  .main-form-wrapper .form-input {
+    width: 30%;
+    margin: 30px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    height: 40px;
+    padding-left: 8px;
+  }
+  .main-form-wrapper .form-submit {
+    background: #40b983;
+    color: #fff;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 5px;
   }
 </style>
